90f2c93ced6153d0070700226a1e8a28652c115e
jawaka345 <lu.k@p2p.legal>
2023-07-30 15:11:56 +0200

    vect: clone and rotate

--- src/vect.ts
+++ src/vect.ts
@@ -10,9 +10,16 @@ export class Vect {
     }
 
     /**
+     * Returns a Vect which is a copy of `this`.
+     */
+    clone(): Vect {
+        return new Vect(this.x, this.y);
+    }
+
+    /**
      * Returns the norm of `this`.
      */
-    norm() {
+    norm(): number {
         return Math.sqrt(this.x ** 2 + this.y ** 2);
     }
 
@@ -50,5 +57,16 @@ export class Vect {
      */
     scalarProd(other: Vect): number {
         return this.x * other.x + this.y * other.y;
+    }
+
+    /**
+     * Rotates `this` Vect.
+     * @param angle is is radians (3.14 or Math.PI for an half circle)
+     */
+    rotate(angle: number){
+        const a = Math.cos(angle)*this.x + Math.sin(angle)*this.y;
+        const b = Math.sin(angle)*this.x - Math.cos(angle)*this.y;
+        this.x = a;
+        this.y = b;
     }
 }
